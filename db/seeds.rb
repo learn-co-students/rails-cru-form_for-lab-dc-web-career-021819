93638947026ae020c69521d6720b4bb9813bac0b
Mickey <micksheridan.24@gmail.com>
2019-03-16 18:30:26 -0400

    submit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+a = Artist.create(name: "Radiohead", bio: "Very mopey")
+g = Genre.create(name: "Doomercore")
+Song.create(name: "Something Mopey", artist_id: a.id, genre_id: g.id)
+
+a2 = Artist.create(name: "The Smiths", bio: "Just as mopey")
+g2 = Genre.create(name: "Classic Sadboi")
+Song.create(name: "Major Key Mopes", artist_id: a2.id, genre_id: g2.id)
